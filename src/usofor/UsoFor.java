8bc829f57876b6a4792f3486ebf5e3208a815928
joserpmx <joserp@hotmail.com>
2015-02-12 11:36:46 -0700

    Agregando Archivos iniciales del proyecto

--- src/usofor/UsoFor.java
+++ src/usofor/UsoFor.java
@@ -14,6 +14,7 @@ public class UsoFor {
      * @param args the command line arguments
      */
      //agrego comentario
+    //Agrego otro Comentario
     public static void main(String[] args) {
         // TODO code application logic here
         int acumulador=0;
